79fd460022dec0037c242d3c6469cbf8603f811b
Matthew Rahtz <mrahtz@google.com>
2023-02-24 19:02:23 +0000

    First skeleton of NumPy support
    PiperOrigin-RevId: 512113538

---
+++ tensor_annotations/numpy.py
@@ -0,0 +1,96 @@
+# Copyright 2023 DeepMind Technologies Limited. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+"""Custom tensor classes for NumPy supporting shape parameterisation.
+
+Note that these should only be used for the purposes of type annotation and
+should never be instantiated. (Certain IDEs may also use these for
+autocompletion, too.)
+
+Type annotations for these classes are maintained in a separate stubs file,
+`numpy.pyi`.
+"""
+
+# LINT.IfChange
+
+from typing import Generic, TypeVar
+
+from tensor_annotations import axes
+
+A1 = TypeVar('A1', bound=axes.Axis)
+A2 = TypeVar('A2', bound=axes.Axis)
+A3 = TypeVar('A3', bound=axes.Axis)
+A4 = TypeVar('A4', bound=axes.Axis)
+A5 = TypeVar('A5', bound=axes.Axis)
+A6 = TypeVar('A6', bound=axes.Axis)
+A7 = TypeVar('A7', bound=axes.Axis)
+A8 = TypeVar('A8', bound=axes.Axis)
+
+
+# We need to define DTypes ourselves rather than use e.g. jnp.uint8 because
+# pytype sees NumPy's own DTypes as `Any`.
+class DType:
+  pass
+
+DT = TypeVar('DT', bound=DType)
+
+
+class _ArrayBase:
+  """Base class for ArrayN classes containing common methods and attributes."""
+
+  def __new__(cls, *args, **kwargs):
+    raise TypeError('tensor_annotations tensors should not be instantiated')
+
+
+class Array1(Generic[DT, A1], _ArrayBase):
+  """An array of rank 1."""
+  pass
+
+
+class Array2(Generic[DT, A1, A2], _ArrayBase):
+  """An array of rank 2."""
+  pass
+
+
+class Array3(Generic[DT, A1, A2, A3], _ArrayBase):
+  """An array of rank 3."""
+  pass
+
+
+class Array4(Generic[DT, A1, A2, A3, A4], _ArrayBase):
+  """An array of rank 4."""
+  pass
+
+
+class Array5(Generic[DT, A1, A2, A3, A4, A5], _ArrayBase):
+  """An array of rank 5."""
+  pass
+
+
+class Array6(Generic[DT, A1, A2, A3, A4, A5, A6], _ArrayBase):
+  """An array of rank 6."""
+  pass
+
+
+class Array7(Generic[DT, A1, A2, A3, A4, A5, A6, A7], _ArrayBase):
+  """An array of rank 7."""
+  pass
+
+
+class Array8(Generic[DT, A1, A2, A3, A4, A5, A6, A7, A8], _ArrayBase):
+  """An array of rank 8."""
+  pass
+
+
+# LINT.ThenChange(numpy.pyi)
---
+++ tensor_annotations/numpy.pyi
@@ -0,0 +1,64 @@
+"""Type stubs for custom NumPy tensor classes.
+
+NOTE: This file is generated from templates/numpy_tensors.pyi.
+
+To regenerate, run the following from the tensor_annotations directory:
+   tools/render_tensor_template.py \
+     --template templates/numpy_tensors.pyi \
+     --out numpy.pyi
+"""
+
+from typing import TypeVar, Generic
+from tensor_annotations.axes import Axis
+
+
+A1 = TypeVar('A1', bound=Axis)
+A2 = TypeVar('A2', bound=Axis)
+A3 = TypeVar('A3', bound=Axis)
+A4 = TypeVar('A4', bound=Axis)
+A5 = TypeVar('A5', bound=Axis)
+A6 = TypeVar('A6', bound=Axis)
+A7 = TypeVar('A7', bound=Axis)
+A8 = TypeVar('A8', bound=Axis)
+
+# We need to define DTypes ourselves rather than use e.g. np.uint8 because
+# pytype sees NumPy's own DTypes as `Any``.
+class DType: pass
+
+DT = TypeVar('DT', bound=DType)
+
+
+class Array0(Generic[DT]):
+  pass
+
+
+class Array1(Generic[DT, A1]):
+  pass
+
+
+class Array2(Generic[DT, A1, A2]):
+  pass
+
+
+class Array3(Generic[DT, A1, A2, A3]):
+  pass
+
+
+class Array4(Generic[DT, A1, A2, A3, A4]):
+  pass
+
+
+class Array5(Generic[DT, A1, A2, A3, A4, A5]):
+  pass
+
+
+class Array6(Generic[DT, A1, A2, A3, A4, A5, A6]):
+  pass
+
+
+class Array7(Generic[DT, A1, A2, A3, A4, A5, A6, A7]):
+  pass
+
+
+class Array8(Generic[DT, A1, A2, A3, A4, A5, A6, A7, A8]):
+  pass
